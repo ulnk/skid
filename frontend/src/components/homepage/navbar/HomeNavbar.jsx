2a2747ac99f0c9a0809f369e4a89e19a8c30cf34
ulnkos <fcongreen@gmail.com>
2022-04-17 21:59:30 +0100

    fix link

--- frontend/src/components/homepage/navbar/HomeNavbar.jsx
+++ frontend/src/components/homepage/navbar/HomeNavbar.jsx
@@ -8,9 +8,9 @@ const HomeNavbar = () => {
         <div className="home-navbar">
             <div className="left-items">
                 <h1 className="left-title"><span className="title-color-change">skid</span>.rocks <span className='smol'>v1.2.1</span></h1>
-                <a href="https://github.com/ulnkos/skidcordprod" className="left-item">Features</a>
-                <a href="https://github.com/ulnkos/skidcordprod" className="left-item">GitHub</a>
-                <a href="https://github.com/ulnkos/skidcordprod" className="left-item">About</a>
+                <a href="https://github.com/ulnk/skid" className="left-item">Features</a>
+                <a href="https://github.com/ulnk/skid" className="left-item">GitHub</a>
+                <a href="https://github.com/ulnk/skid" className="left-item">About</a>
             </div>
             <div className="right-items">
                 <Link to="/login"><button className="login-button">Login</button></Link>
