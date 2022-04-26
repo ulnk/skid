95eabef323aa2c11cb4af592951d69c3da5e436c
ulnkos <fcongreen@gmail.com>
2022-04-26 18:48:41 +0100

    i forgot how react works

--- backend/index.js
+++ backend/index.js
@@ -6,16 +6,18 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const rateLimit = require('express-rate-limit');
 const session = require('express-session');
+const path = require('path');
 const { io } = require('./util/socket.js');
 
 const PORT = process.env.PORT || 5001;
-const RATE_LIMIT = 120;
+const RATE_LIMIT = parseInt(process.env.RATE_LIMIT) || 120;
 const DOMAINS = ['http://localhost:3000', 'https://skid.rocks', 'https://skid.today', undefined, 'chrome-extension://gmmkjpcadciiokjpikmkkmapphbmdjok'];
 
 const apiRouter = require('./routes/index.js');
 ;(async () => {
     await mongooose.connect(process.env.MONGODB);
     const app = express() 
+        .use(express.static(path.join(__dirname, 'build')))
         .use(cors({origin: (origin, callback) => { if (DOMAINS.indexOf(origin) !== -1)  callback(null, true) }}))
         .use(express.json())
         .use(bodyParser.urlencoded({ extended: false }))
@@ -23,6 +25,7 @@ const apiRouter = require('./routes/index.js');
         .use(rateLimit({ windowMs: 5 * 1000, max: RATE_LIMIT, message: "lil skid try and ddos. smoking on you 🚬🚬🚬🚬. https://discord.gg/horion2" }))
         .use(session({ secret: 'skid', resave: false, saveUninitialized: false, cookie: { expires: 60 * 60 * 24 } }))
         .use('/api', apiRouter)
+        .get('/*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')))
         .listen(PORT, () => console.log(`[LOADED EXPRESS] PORT ${PORT}`));
     
     io(app, () => {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const router = express.Router();
 ;(async() => {
     for (let file of fs.readdirSync(__dirname)) {
-        if (fs.lstatSync(`${__dirname}\\${file}`).isFile()) continue;
-        router.use(`/${file}`, require(`${__dirname}\\${file}\\index.js`));
+        if (fs.lstatSync(`${__dirname}/${file}`).isFile()) continue;
+        router.use(`/${file}`, require(`${__dirname}/${file}/index.js`));
     }
 })().catch();
 
