fa7155593fa0172db92ba6196f88421be1cf774f
ulnkos <fcongreen@gmail.com>
2022-05-16 18:58:33 +0100

    a

--- frontend/src/api/message/createMessage.js
+++ frontend/src/api/message/createMessage.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const URI = process.env.NODE_ENV === 'production' ? 'https://skid.today' : 'http://localhost:5001';
 
-export const createMessage = (content, serverId, categoryId, channelId, small) => content && serverId && categoryId && channelId ? axios.post(URI + `/api/v1/message/createMessage`, { content, serverId, categoryId, channelId, small }, {
+export const createMessage = (content, server, category, channel, small) => content && server && category && channel ? axios.post(URI + `/api/v1/message/createMessage`, { content, server, category, channel, small }, {
     headers: {
         'x-auth-token': localStorage.getItem("token"),
     }
