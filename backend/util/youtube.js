d61fba0011b8f0c04a25a6afb1d495cab68581b5
ulnkos <fcongreen@gmail.com>
2022-05-22 17:16:41 +0100

    fix bug

--- backend/util/youtube.js
+++ backend/util/youtube.js
@@ -3,12 +3,17 @@ const request = require('request');
 const getYoutubeData = async (id) => {
     const URI = `https://www.googleapis.com/youtube/v3/videos?id=${id}&key=${process.env.YT}&part=snippet,contentDetails,statistics,status`;
     return new Promise((resolve, reject) => {
-        request(URI, (err, response, body) => {
-            if (err) return reject(err);
-            const data = JSON.parse(body);
-            if (data.items.length === 0) return reject(new Error('No data found'));
-            resolve(data.items[0].snippet);
-        });
+        try {
+
+            request(URI, (err, response, body) => {
+                if (err) return reject(err);
+                const data = JSON.parse(body);
+                if (!data) return reject(new Error('No data found'));
+                resolve(data.items[0].snippet);
+            });
+        } catch (e) {
+            reject(e);
+        }
     });
 };
 
